e4c29c1f1d92af84390060eb6d5e1f553bbdc766
sanjay14073 <103362239+sanjay14073@users.noreply.github.com>
2024-11-12 18:28:00 +0530

    Added basic folder structure

---
+++ src/components/Sponsors.jsx
@@ -0,0 +1,10 @@
+function Sponsors() {
+    return (
+        <div>
+        <h1>This is Sponsors section</h1>
+       </div>
+    );
+  }
+  
+  export default Sponsors
+;
---
+++ src/pages/MainPage.jsx
@@ -0,0 +1,25 @@
+import About from "../components/About";
+import Contact from "../components/Contact";
+import Domains from "../components/Domains";
+import FAQ from "../components/Faq";
+import Home from "../components/Home";
+import Sponsors from "../components/Sponsors";
+import Teams from "../components/Teams";
+import Timeline from "../components/Timeline";
+
+function MainPage() {
+    return (
+        <>
+        <Home/>
+        <About/>
+        <Domains/>
+        <Timeline/>
+        <Teams/>
+        <Sponsors/>
+        <FAQ/>
+        <Contact/>
+        </>
+    );
+  }
+  
+  export default MainPage;
