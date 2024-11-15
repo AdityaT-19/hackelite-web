fe6bd1823fd23f5b34fd9f09b3b6de194f8bb9b2
Vaibhav-Kumar-K-R <vaibhavkumar.k.r.cnr@gmail.com>
2024-11-15 22:09:35 +0530

    Required changes done related to spellings

--- src/constants/faqdata.js
+++ src/constants/faqdata.js
@@ -1,11 +1,11 @@
  const  faqs = [
   {
     question: "What is Hackelite '24?",
-    answer: "HackElite '24 is a 24-hour national-level hackathon organized by the Developer Student Club (DSC) and Linux Campus Club (LCC) at Sri Jayachamarajendra College of Engineering. This event is designed to bring students, developers, and tech enthusiasts together to collaborate on real-world challenges, foster innovation, and showcase solutions leveraging cutting-edge technologies."
+    answer: "Hackelite '24 is a 24-hour national-level hackathon organized by the Developer Student Club (DSC) and Linux Campus Club (LCC) at Sri Jayachamarajendra College of Engineering. This event is designed to bring students, developers, and tech enthusiasts together to collaborate on real-world challenges, foster innovation, and showcase solutions leveraging cutting-edge technologies."
   },
   {
     question: "Who can participate in Hackelite '24?",
-    answer: "HackElite '24 is open to students, developers, and tech enthusiasts from all over India. Participants of various skill levels are welcome, as hackathons are a great learning experience, whether you’re a beginner or an experienced coder."
+    answer: "Hackelite '24 is open to students, developers, and tech enthusiasts from all over India. Participants of various skill levels are welcome, as hackathons are a great learning experience, whether you’re a beginner or an experienced coder."
   },
   {
     question: "How many members can be in a team?",
@@ -17,7 +17,7 @@
   },
   {
     question: "What are the prizes for the winners?",
-    answer: "HackElite '24 will offer exciting prizes and awards to the top teams. Prizes may include cash awards, tech gadgets, internship opportunities, and recognition. Exact details will be shared closer to the event."
+    answer: "Hackelite '24 will offer exciting prizes and awards to the top teams. Prizes may include cash awards, tech gadgets, internship opportunities, and recognition. Exact details will be shared closer to the event."
   },
   {
     question: "What do I need to bring to the hackathon?",
