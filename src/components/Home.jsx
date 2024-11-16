92ffefa080919319002e047560c59b4af3fa7d72
Vasudevshetty <vasudeepu2815@gmail.com>
2024-11-16 20:29:14 +0530

    Added last round of design elements to the home page, done with the seciton completely, changes to the position, attributes to the galaxy may take place .

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,13 +1,30 @@
 import Hero from "./Hero";
 import Navbar from "./Navbar";
-import { Vortex } from "./Vortex";
+import { Canvas } from "@react-three/fiber";
+import Galaxy from "./Galaxy";
+import CountdownTimer from "./CountdownTimer";
 
 function Home() {
   return (
-    <Vortex rangeY={800} particleCount={800} baseHue={120}>
+    <>
+      {/* Canvas Background */}
+      <div className="absolute top-0 left-0 w-full h-full -z-10 ">
+        <div className="absolute flex h-[80vh] w-full">
+          <div className="radial-gradient-bg h-full w-full" />
+          <div className="radial-gradient-bg h-full w-full" />
+          <div className="radial-gradient-bg h-full w-full" />
+        </div>
+
+        <Canvas camera={{ position: [0, 0, 15], fov: 75 }}>
+          <Galaxy />
+        </Canvas>
+      </div>
+
+      {/* Foreground Sections */}
       <Navbar />
       <Hero />
-    </Vortex>
+      <CountdownTimer />
+    </>
   );
 }
 
