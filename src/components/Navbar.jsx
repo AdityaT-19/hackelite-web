2f9898d0b2d469ab2b274671ac16b4f42cc8093e
Vasudevshetty <vasudeepu2815@gmail.com>
2024-11-20 09:50:08 +0530

    Removed particles animations in timeline component to ensure proper 3d effect of three.js in hero section. 2nd round of optmisations and considerations has to be done.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -19,7 +19,7 @@ function Navbar() {
   };
 
   return (
-    <nav id="nav" className="h-18 md:h-14 flex justify-between relative">
+    <nav id="nav" className="h-18 md:h-14 flex justify-between sticky top-0 z-50">
       <div className="w-[40%] bg-primary-green bg-opacity-[22%] h-full clip-left hidden md:flex">
         <ul className="flex text-white items-center justify-evenly w-full text-lg max-lg:text-sm pr-[60px] font-semibold">
           {navLink1.map((link, index) => (
