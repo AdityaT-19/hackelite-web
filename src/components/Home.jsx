c9f21e002bae49f5a3abe7378b2fd96eafc62ecf
Vasudevshetty <vasudeepu2815@gmail.com>
2024-12-13 14:30:19 +0530

    Addeed popup

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,46 @@
 import { Canvas } from "@react-three/fiber";
 import Galaxy from "./Galaxy";
 import CountdownTimer from "./CountdownTimer";
+import { useState, useEffect } from "react";
+import Modal from "./Info_Popover";
 
 function Home() {
+  const [isInfo, setIsInfo] = useState(false);
+  const [title, setTitle] = useState("");
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    // Function to check the current time
+    const checkTime = () => {
+      const now = new Date();
+      const currentHour = now.getHours();
+      const currentMinute = now.getMinutes();
+
+      // Show initial message until 3:00 PM
+      if (currentHour === 14 && currentMinute >= 20) {
+        setIsInfo(true);
+        setTitle("It's Lunch Time");
+        setMessage(
+          "Lunch will be open till 3:00. First checkpoint will be at 3:00. Half of the team will stay back while the other half please have your lunch."
+        );
+      }
+
+      // Change modal content at 3:00 PM
+      if (currentHour >= 15 && currentMinute >= 0) {
+        setTitle("1st Checkpoint Reminder");
+        setMessage(
+          "First checkpoint has started. Please make sure to be ready for your turn."
+        );
+      }
+    };
+
+    // Check every minute
+    const interval = setInterval(checkTime, 1000);
+
+    // Cleanup interval on component unmount
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <div className="flex flex-col items-center justify-center">
       {/* Canvas Background */}
@@ -20,6 +58,12 @@ function Home() {
 
       {/* Foreground Sections */}
       <CountdownTimer />
+      <Modal
+        isVisible={isInfo}
+        title={title}
+        message={message}
+        onClose={() => setIsInfo(false)}
+      />
     </div>
   );
 }
